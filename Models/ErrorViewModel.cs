c68e1dac9551d43ac630bf8f70065a6a3b363bb0
Marco <riosarone99@gmail.com>
2022-02-25 22:40:20 -0500

    Proyecto Agregado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BCP_CRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
